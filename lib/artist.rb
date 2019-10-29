374978c335c7057f41216698977051a49e47fe99
Pancho Labrador <plabradorjr@gmail.com>
2019-10-29 04:32:57 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.find_by_artist(self)
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def self.create(name)
+      self.new(name).tap {|artist| artist.save}
+  end
+
+  def self.find(name)
+      self.all.find {|artist| artist.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+      self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def print_songs
+      songs.each {|song| puts song.name}
+  end
+
+  def save
+    @@all << self
+  end
+
+
+end
